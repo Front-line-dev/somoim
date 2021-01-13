50c14bdb33c21cc8aae3b47fbd41980e0b3ebd26
Front-line-dev <kimminsung@kakao.com>
2021-01-13 12:31:00 +0900

    feat: receive stream

--- client/main.js
+++ client/main.js
@@ -1,9 +1,33 @@
-document.querySelector('#join-button').addEventListener('click', () => {
-    document.querySelector('main').classList.add('hidden-div')
-    document.querySelector('.client').classList.remove('hidden-div')
+const qs = document.querySelector.bind(document)
+
+const peer = new Peer()
+
+// Receive call
+peer.on('call', (stream) => {
+    const video = qs('#client-video')
+
+    stream.on('stream', (stream) => {
+        video.srcObject = stream
+        video.addEventListener('loadedmetadata', () => {
+            video.play()
+        })
+    })
+
+    stream.on('close', () => {
+        video.remove()
+    })
 })
 
-document.querySelector('#host-button').addEventListener('click', () => {
-    document.querySelector('main').classList.add('hidden-div')
-    document.querySelector('.host').classList.remove('hidden-div')
+qs('#join-button').addEventListener('click', () => {
+    qs('main').classList.add('hidden-div')
+    qs('.client').classList.remove('hidden-div')
+})
+
+qs('#host-button').addEventListener('click', () => {
+    qs('main').classList.add('hidden-div')
+    qs('.host').classList.remove('hidden-div')
+})
+
+qs('#select-screen-button').addEventListener('click', () => {
+
 })
